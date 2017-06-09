58d871e8194633461088ec6f42845e8c983ecfd9
Brian Dorfman <bdorfman@stripe.com>
2017-06-09 14:47:38 -0700

    Validate and format US codes more accurately.
    Code in knowledge of zip and zip+4 for better validation/formatting.

--- Stripe/STPAddress.m
+++ Stripe/STPAddress.m
@@ -247,8 +247,8 @@ NSString *stringIfHasContentsElseNil(NSString *string);
         case STPBillingAddressFieldsNone:
             return YES;
         case STPBillingAddressFieldsZip:
-            return [STPPostalCodeValidator stringIsValidPostalCode:self.postalCode 
-                                                       countryCode:self.country];
+            return ([STPPostalCodeValidator validationStateForPostalCode:self.postalCode
+                                                            countryCode:self.country] == STPCardValidationStateValid);
         case STPBillingAddressFieldsFull:
             return [self hasValidPostalAddress];
     }
@@ -277,8 +277,8 @@ NSString *stringIfHasContentsElseNil(NSString *string);
             && self.city.length > 0 
             && self.country.length > 0 
             && (self.state.length > 0 || ![self.country isEqualToString:@"US"])  
-            && [STPPostalCodeValidator stringIsValidPostalCode:self.postalCode 
-                                                   countryCode:self.country]);
+            && ([STPPostalCodeValidator validationStateForPostalCode:self.postalCode
+                                                         countryCode:self.country] == STPCardValidationStateValid));
 }
 
 + (PKAddressField)applePayAddressFieldsFromBillingAddressFields:(STPBillingAddressFields)billingAddressFields {
